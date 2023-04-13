e2209345912502f88bda9b86e47f22c090f55b86
Blade Coates <blade@coates.life>
2023-04-12 20:54:09 -0400

    RSS: Stylr sheet and schema updates

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,7 +6,7 @@ export async function get(context) {
   return data.draft === false;
 });
   return rss({
-    customData: '<atom:link href="https://bladecoates.dev/rss.xml" rel="self" type="application/rss+xml" />',
+    //customData: '<atom:link href="https://bladecoates.dev/rss.xml" rel="self" type="application/rss+xml" />',
     stylesheet: '/rss/pretty-feed-v3.xsl',
     title: 'Blade Coates Blog',
     description: 'Constantly evolving my problem-solving, development, and leadership skills.',
