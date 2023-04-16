4401920b67f0bc9ac3302b36f6f26fd26373a645
Blade Coates <blade@coates.life>
2023-04-15 23:36:49 -0400

    Feature: Link module, links I found interesting while browsing the web

---
+++ src/components/Cards/LinkSummary.svelte
@@ -0,0 +1,71 @@
+<script>
+	import "agnostic-svelte/css/common.min.css";
+	import { Card, Tag } from "agnostic-svelte";
+
+	// export let isMobile;
+
+	export let link;
+    export let title;
+    export let summary;
+</script>
+<Card>
+		<div>
+			<ul id="arround-web-links" class="list-none">
+				<li>
+					<details>
+						<summary><a href={link}>{title}</a></summary>
+						{summary}
+					</details>
+				</li>
+			</ul>
+		</div>
+</Card>
+<!--<Card isBorder={false} isStacked={true} css="bg-theme-paper py-1 px-3">
+	<div class="w-full content-start font-semibold">
+		<a href={slug}>{title}</a>
+	</div>
+	<div class="w-full content-start">{description}</div>
+	<div class="w-full">
+		<Card css="">
+			<div class="flex w-full grid-cols-4 content-center items-stretch gap-2">
+				<div class="shrink grow-0">
+					Author: <a href="/about#self">Blade Coates</a>
+				</div>
+				<div class="flex shrink-0 grow justify-end">
+					Date:&nbsp;<time class="" datetime={date.toISOString()}
+						>{date.toLocaleDateString("en-us", {
+							year: "numeric",
+							month: "short",
+							day: "numeric",
+						})}</time
+					>
+				</div>
+			</div>
+		</Card>
+		<Card css="">
+			<div class="grid w-full grid-cols-2 content-center items-stretch gap-2">
+				<div class="shrink grow-0">Tags</div>
+				<div class="flex shrink-0 grow justify-end gap-2" style="">
+					<a href="/" class="tag"><Tag>tag-x</Tag></a><a href="/" class="tag"
+						><Tag>tag-y</Tag></a
+					>
+				</div>
+			</div>
+		</Card>
+	</div>
+</Card>-->
+
+<style>
+	div > a.tag {
+		text-decoration: none;
+		border: 1px solid var(--theme-border-color);
+	}
+	div > a.tag:hover {
+		color: var(--theme-accent);
+		border: 1px solid var(--theme-accent);
+	}
+
+	/* .infobox-mobile-container {
+    --header-bottom-padding: 1.5rem;
+  } */
+</style>
