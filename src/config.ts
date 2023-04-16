32222eab960aca49445fabfa91b4de1c47f76849
Blade Coates <blade@coates.life>
2023-04-16 09:28:52 -0400

    Comments: Giscus

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,22 @@ export const SITE = {
   giscus_github_repo_id: "R_kgDOJVt0Wg"
 };
 
+export const giscus = {
+  repo: "bladecoates/bladecoates-dev",
+  repoId: "R_kgDOJVt0Wg",
+  category: "category",
+  categoryId: "DIC_kwDOJVt0Ws4CV0HV",
+  mapping: "pathname",
+  strict: "1",
+  term: "Welcome to @giscus/react component!",
+  reactionsEnabled: "1",
+  emitMetadata: "0",
+  inputPosition: "bottom",
+  theme: "dark_tritanopia",
+  lang: "en",
+  loading: "lazy",
+}
+
 // This is the type of the Frontmatter you put in the docs markdown files.
 export type Frontmatter = {
   title: string;
