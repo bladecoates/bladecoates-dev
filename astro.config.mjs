ada36015508460214695da10a07da5807d4f4cbd
Blade Coates <blade@coates.life>
2024-02-24 00:11:50 -0500

    Update intergrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
-import sitemap from "@astrojs/sitemap";
 
 import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
   site: "https://bladecoates.dev",
   trailingSlash: "never",
-  integrations: [mdx(), svelte(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), sitemap()],
+  integrations: [mdx(), tailwind(), svelte(), sitemap()],
   vite: {
     plugins: [],
     resolve: {
@@ -34,9 +31,9 @@ export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
       langs: [],
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-  },
-  },
+      wrap: true
+    }
+  }
   // output: "server",
   // adapter: node({
   //  mode: "standalone"
